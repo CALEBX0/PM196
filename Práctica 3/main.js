10e5d70b8fa34ffa0bcbbd1f333c0371f3e4d969
Calebxo <angelcalebhinojosaherrera@gmail.com>
2025-05-25 23:05:33 -0600

    Ejercicio b-3
    Verificar usuario

---
+++ Práctica 3/main.js
@@ -0,0 +1,19 @@
+import { restar } from "./utils";
+
+function verificarUsuario(usuario) {
+    return new Promise((resolve, reject) => {
+        if (usuario === "admin") {
+            resolve("Acceso concedido");
+        } else {
+            reject("Acceso denegado");
+        }
+    });
+}
+
+verificarUsuario("admin")
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
+
+verificarUsuario("caleb")
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
