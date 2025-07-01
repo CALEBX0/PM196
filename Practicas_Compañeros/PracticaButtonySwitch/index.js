7846501efbff2f808564c5ca5268c20786fcc052
Calebxo <angelcalebhinojosaherrera@gmail.com>
2025-07-01 15:32:05 -0600

    FlatList
    PokeList

--- Practicas_Compañeros/PracticaButtonySwitch/index.js
+++ Practicas_Compañeros/PracticaButtonySwitch/index.js
@@ -1,6 +1,6 @@
 import { registerRootComponent } from 'expo';
 
-import App from './scrollview.js';
+import App from './FlatlistSectionlist.js';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
